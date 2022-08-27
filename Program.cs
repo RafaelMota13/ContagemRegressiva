d1668c2ca9c594cb22edc2b57e661a8e4987c5f7
Rafael <faelmota7@gmail.com>
2022-08-26 22:28:56 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Prepar...");
+Console.WriteLine();
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.WriteLine();
+Console.ReadKey();
+Console.Write("FOGO !");
+Console.WriteLine();
+
